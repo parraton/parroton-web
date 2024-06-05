31cf08bce74050cb772e9003505a1f6e7daa6a84
digberi <orkepuma@gmail.com>
2024-06-05 15:04:37 +0300

    return all

--- src/core/functions/deposit-pool.ts
+++ src/core/functions/deposit-pool.ts
@@ -2,9 +2,7 @@ import {toNano, OpenedContract} from '@ton/core';
 import {AssetType, JettonRoot, Pool, PoolType, VaultJetton} from '@dedust/sdk';
 import {DeDustFactory} from '../contracts/dedust-factory';
 import {HOLE_ADDRESS, tonClientPromise} from '../config';
-import {Sender, successTransaction} from "@utils/sender";
-import {lastValueFrom} from "rxjs";
-import {toast} from "sonner";
+import {Sender} from "@utils/sender";
 
 export async function depositPool(
   dedustFactory: OpenedContract<DeDustFactory>,
@@ -28,21 +26,17 @@ export async function depositPool(
   const nativeVault = tonClient.open(await dedustFactory.getNativeVault());
   const jettonVault = tonClient.open(await dedustFactory.getJettonVault(jettonAddress));
 
-  await nativeVault.sendDepositLiquidity(sender, {
+  const tonDepositResult = await nativeVault.sendDepositLiquidity(sender, {
     poolType: PoolType.VOLATILE,
     assets: assets,
     targetBalances: assetAmounts,
     amount: tonAmount,
   });
 
-  const depositLiquidityHash = await lastValueFrom(successTransaction)
-
-  toast.success(`Deposit liquidity transaction hash: ${depositLiquidityHash}`);
-
   const jettonRoot = tonClient.open(JettonRoot.createFromAddress(jettonAddress));
   const investorJettonWallet = tonClient.open(await jettonRoot.getWallet(sender.address!));
 
-  await investorJettonWallet.sendTransfer(sender, toNano('1.2'), {
+  const jettonTransferResult = await investorJettonWallet.sendTransfer(sender, toNano('1.2'), {
     amount: jettonAmount,
     destination: jettonVault.address,
     responseAddress: sender.address!,
@@ -54,8 +48,5 @@ export async function depositPool(
     }),
   });
 
-  const jettonTransferHash = await lastValueFrom(successTransaction)
-
-  toast.success(`Jetton transfer transaction hash: ${jettonTransferHash}`);
-  toast.success(`Pool deposited successfully: ${tonAmount} TON and ${jettonAmount} jUSDT`);
+  return {tonDepositResult, jettonTransferResult};
 }
