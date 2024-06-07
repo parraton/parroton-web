dc1325de9628c29d72bc8594f93337c75f58ac6a
digberi <orkepuma@gmail.com>
2024-06-07 16:14:07 +0300

    add next/image

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,6 +4,7 @@ import { Home } from '@routes';
 import { Settings } from '@components/settings';
 import { Language } from '@i18n/settings';
 import { ConnectWallet } from '@components/connect-wallet';
+import Image from 'next/image';
 
 export async function Navbar({ lng }: { lng: Language }) {
   const { t } = await serverTranslation(lng, 'common');
@@ -12,7 +13,7 @@ export async function Navbar({ lng }: { lng: Language }) {
     <nav className={cn('flex h-24 justify-between bg-lime-200')}>
       <div className={cn('container flex items-center justify-between')}>
         <Home.Link className={cn('flex items-center gap-2')}>
-          <img src='/images/logo.png' alt='Home' width={24} height={24} />
+          <Image src='/images/logo.png' alt='Home' width={24} height={24} />
           <span>{t('app_title')}</span>
         </Home.Link>
 
