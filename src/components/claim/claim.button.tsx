4aa810523beba2dcc478beecc7fcbe3faa68b579
Zhanna Bieliavska <zhanna.bieliavska@nixsolutions.com>
2024-06-08 21:43:44 +0300

    rewards translate

--- src/components/claim/claim.button.tsx
+++ src/components/claim/claim.button.tsx
@@ -10,8 +10,11 @@ import { useParams } from '@routes/hooks';
 import { VaultPage } from '@routes';
 import { Address } from '@ton/core';
 import { claim } from '@components/claim/claim';
+import { useTranslation } from '@i18n/client';
 
 export function ClaimButton() {
+  const { t } = useTranslation({ ns: 'common' });
+
   const { vault } = useParams(VaultPage);
   const { sender } = useConnection();
 
@@ -30,7 +33,7 @@ export function ClaimButton() {
 
   return (
     <Button onClick={handleClaim} className='custom-main-btn'>
-      Claim Rewards
+      {t('rewards')}
     </Button>
   );
 }
