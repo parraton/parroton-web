39c4e8828a350215f81650b1b50cd57ee668e734
digberi <orkepuma@gmail.com>
2024-06-09 09:22:32 +0300

    fix language change

--- src/components/toggle-language/use-change-language.ts
+++ src/components/toggle-language/use-change-language.ts
@@ -1,19 +1,16 @@
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { useParams } from '@routes/hooks';
 import { Home } from '@routes';
 import { Language, languages } from '@i18n/settings';
 
 export const useChangeLanguage = () => {
-  const router = useRouter();
   const pathname = usePathname();
   const { lng } = useParams(Home);
 
   const langs = languages.filter((lang) => lang !== lng);
 
   const changeLanguage = (lang: string) => {
-    const newPath = pathname.replace(`/${lng}`, `/${lang}`);
-
-    router.push(newPath, { scroll: false });
+    window.location.pathname = pathname.replace(`/${lng}`, `/${lang}`);
   };
 
   return { currentLanguage: lng as Language, languages: langs, changeLanguage };
