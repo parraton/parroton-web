db56bcb64f6e6dab3ff09fdb4f80f729ab5daafb
Mega1on <mega1on@proton.me>
2024-09-05 21:46:27 +0100

    add stats API route (with cache&retries)

---
+++ src/app/api/vaults/route.info.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+export const Route = {
+  name: "VaultsGet",
+  params: z.object({}),
+  search: z.object({}),
+};
+
+export const GET = {
+  result: z.array(
+    z.object({
+      name: z.string(),
+      vaultAddress: z.string(),
+      plpMetadata: z.object({
+        address: z.string(),
+        name: z.string(),
+        symbol: z.string(),
+        decimals: z.string(),
+        image: z.string(),
+        description: z.string(),
+      }),
+      lpMetadata: z.object({
+        address: z.string(),
+        name: z.string(),
+        symbol: z.string(),
+        decimals: z.string(),
+        image: z.string(),
+      }),
+      lpPriceUsd: z.string(),
+      plpPriceUsd: z.string(),
+      tvlUsd: z.string(),
+      apr: z.string(),
+      apy: z.string(),
+      dailyUsdRewards: z.string(),
+    })
+  ),
+};
