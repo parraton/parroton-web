1f6a65a02b6d20216472c04d4e72d6ef8fee2e23
Inokentii Mazhara <keshan3262@gmail.com>
2024-09-14 21:59:22 +0300

    Remove one get_strategy_data request

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -10,11 +10,12 @@ const mainnetTonViewerUrl = 'https://tonviewer.com';
 const mainnetTonApiUrl = 'https://tonapi.io';
 const mainnetTonCenterUrl = 'https://toncenter.com/api/v2/jsonRPC';
 
-const defaultVaultsApi = 'https://parraton-api-yklcg.ondigitalocean.app/v1/vaults';
+const mainnetVaultsApi = 'https://parraton-api-yklcg.ondigitalocean.app/v1/vaults';
+const testnetVaultsApi = 'https://mock-testnet-api-b3bbi.ondigitalocean.app/v1/vaults';
 
 export const IPFS_GATEWAY = 'https://gateway.pinata.cloud/ipfs';
 export const TON_CLIENT_URL = isMainnet ? mainnetTonClientUrl : testnetTonClientUrl;
 export const TONVIEWER_URL = isMainnet ? mainnetTonViewerUrl : testnetTonViewerUrl;
 export const TONAPI_URL = isMainnet ? mainnetTonApiUrl : testnetTonApiUrl;
 export const TONCENTER_URL = isMainnet ? mainnetTonCenterUrl : testnetTonCenterUrl;
-export const VAULTS_API = defaultVaultsApi;
+export const VAULTS_API = isMainnet ? mainnetVaultsApi : testnetVaultsApi;
