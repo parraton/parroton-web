3116f82ca09e5748a73314325dac746e7338db22
Inokentii Mazhara <keshan3262@gmail.com>
2024-09-18 22:09:01 +0300

    Remove Sender class and make 'Claim' button a stub

--- src/components/claim/claim.button.tsx
+++ src/components/claim/claim.button.tsx
@@ -1,29 +1,15 @@
 'use client';
 
 import { Button } from '@UI/button';
-import { useConnection } from '@hooks/use-connection';
-import { toast } from 'sonner';
-import { useParams } from '@routes/hooks';
-import { VaultPage } from '@routes';
-import { Address } from '@ton/core';
-import { claim } from '@components/claim/claim';
 import { useTranslation } from '@i18n/client';
+import { useCallback } from 'react';
 
 export function ClaimButton() {
   const { t } = useTranslation({ ns: 'common' });
 
-  const { vault } = useParams(VaultPage);
-  const { sender } = useConnection();
-
-  const handleClaim = async () => {
-    try {
-      await claim(sender, Address.parse(vault));
-      // TODO: implement status tracking
-    } catch (error) {
-      console.error(error);
-      toast.error('Something went wrong. Please try again later.');
-    }
-  };
+  const handleClaim = useCallback(async () => {
+    console.log('TODO: implement claim');
+  }, []);
 
   return (
     <Button onClick={handleClaim} className='custom-main-btn'>
--- src/components/claim/claim.ts
+++ src/components/claim/claim.ts
@@ -1,9 +1,6 @@
-import { Sender } from '@utils/sender';
 import { DistributionPool } from '@dedust/apiary-v1';
-import { Address, OpenedContract } from '@ton/core';
-import { claimDeDustDistributionRewards } from './claim-de-dust-distribution-rewards';
+import { OpenedContract } from '@ton/core';
 import { fetchDictionaryFromIpfs } from '@components/claim/fetch-dictionary-from-ipfs';
-import { getVaultDistributionPool } from '@hooks/use-vault-distribution-pool';
 
 export const getRewardsDictionary = async (distributionPool: OpenedContract<DistributionPool>) => {
   const { dataUri } = await distributionPool.getRewardsData();
@@ -13,24 +10,4 @@ export const getRewardsDictionary = async (distributionPool: OpenedContract<Dist
   }
 
   return await fetchDictionaryFromIpfs(dataUri);
-};
-
-const claimRewards = async (
-  sender: Sender,
-  distributionPool: OpenedContract<DistributionPool>,
-  userAddress: Address,
-) => {
-  const rewardsDictionary = await getRewardsDictionary(distributionPool);
-
-  await claimDeDustDistributionRewards(sender, rewardsDictionary, distributionPool, userAddress);
-};
-
-export const claim = async (sender: Sender, vaultAddress: Address) => {
-  if (!sender.address) {
-    throw new Error('Sender address is not defined');
-  }
-
-  const distributionPool = getVaultDistributionPool(vaultAddress.toString());
-
-  await claimRewards(sender, distributionPool, sender.address);
 };
